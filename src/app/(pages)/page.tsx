dae4c552d332933e42a470f8df7f9a715b607bae
Jonathan Perry <jpper@umich.edu>
2024-04-24 13:07:29 -0500

    List photos from cloudinary

--- src/app/(pages)/page.tsx
+++ src/app/(pages)/page.tsx
@@ -1,15 +1,19 @@
-import MediaGallery from '@/components/MediaGallery';
+import { v2 as cloudinary } from "cloudinary";
+
+import MediaGallery from "@/components/MediaGallery";
+
+cloudinary.config({
+  cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_NAME,
+  api_key: process.env.NEXT_PUBLIC_CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 export default async function Home() {
+  const { resources } = await cloudinary.api.resources();
+  console.log(resources);
   return (
     <div className="h-full mt-6">
-      <MediaGallery
-        resources={[
-          {
-            id: 'my-image'
-          }
-        ]}
-      />
+      <MediaGallery resources={resources} />
     </div>
-  )
+  );
 }
